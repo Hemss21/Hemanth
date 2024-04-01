dd3ab11c75504c532ac8bf4223d7360c3d5f5b72
Hemss21 <147019493+Hemss21@users.noreply.github.com>
2024-04-01 12:54:31 +0530

    Hemanth Keylogger.py

--- Hemanth Keylogger.py
+++ Hemanth Keylogger.py
@@ -6,7 +6,7 @@ import json
 # Global variables
 keys_used = []
 flag = False
-output_file = 'sundhar key_log.json'
+output_file = 'Hemanth.json'
 
 # Function to generate log files
 def generate_log(data, file_path):
